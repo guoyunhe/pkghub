d9bb0ab971259bb38ea3a7d74ae12450f736d6ec
Guo Yunhe <i@guoyunhe.me>
2026-09-12 14:55:51 +0800

    refactor(pkg): license

--- database/migrations/1788932574694_create_pkgs_table.ts
+++ database/migrations/1788932574694_create_pkgs_table.ts
@@ -20,7 +20,7 @@ export default class extends BaseSchema {
       table.string('version').nullable()
       table.string('release').nullable()
       table.string('arch').nullable()
-      table.string('license').nullable()
+      table.text('license').nullable()
       table.text('summary').nullable()
       table.text('description').nullable()
       table
